41a9bc09f34bab0e2aca8052d089580119fb275e
Arslan <ali.arslan319@gmail.com>
2022-09-17 22:12:35 -0400

    small cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
         <h1>Skylinked</h1>
-      </header>
     </div>
   );
 }
