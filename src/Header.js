4f2d3a1835a6ed94cdc2b37e41729dbafc7255b0
Arslan <ali.arslan319@gmail.com>
2022-09-18 22:31:51 -0400

    install material ui, begin icon implementation in code

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   return (
-    <div className = 'header'>Header</div>
+    <div className ='header'>
+          <div className ='header_left'>
+            <img src='' alt='' />
+          </div> 
+
+          <div className ='header_search'>
+            <SearchIcon />
+            <input type='text' />
+          </div> 
+
+          <div className ='header_right'>
+
+          </div> 
+    </div>
   )
 }
 
