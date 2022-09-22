3bf6d77b7b1580ee0f47893db9b74937efdebde7
Arslan <ali.arslan319@gmail.com>
2022-09-22 01:14:42 -0400

    even more icon and header styling

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,6 +1,6 @@
 import './HeaderOption.css'
 
-const HeaderOption = ({ Icon, title }) => {
+const HeaderOption = ({ avatar, Icon, title }) => {
   return  <div className='headerOption'>
     { Icon && <Icon className='headerOption_Icon' /> }
     <h5 className='headerOption_title'>{ title }</h5>
