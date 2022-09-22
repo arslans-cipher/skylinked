906d1d0d3dfe8976237381f84cc533bb4deb0f25
Arslan <ali.arslan319@gmail.com>
2022-09-22 00:48:23 -0400

    add new Icons for headerRight

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,9 @@ import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home'
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
+import ChatIcon from '@mui/icons-material/Home'
+import NotificationsIcon from '@mui/icons-material/Notifications'
 import HeaderOption from './HeaderOption';
 
 const Header = () => {
@@ -18,7 +21,12 @@ const Header = () => {
 
           <div className ='header_right'>
             <HeaderOption Icon={HomeIcon} title='Home'/>
-            <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+            <HeaderOption Icon={SupervisorAccountIcon} title='Home'/>
+            <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+            <HeaderOption Icon={ChatIcon} title='Messaging'/>
+            <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+
+
           </div> 
     </div>
   )
