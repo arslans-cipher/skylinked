906e5c62e7473b9706b122411d432c5e54e2d3ea
Arslan <ali.arslan319@gmail.com>
2022-09-24 23:49:21 -0400

    import Avatar into Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,15 @@
+import { Avatar } from '@mui/material'
 import './Sidebar.css'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-
+      <div className="sidebar_top">
+        <img src='' alt='' />
+        <Avatar />
+        <h2>Arslan Ali</h2>
+        <h4>Arslan@gmail.com</h4>
+      </div>
     </div>
   )
 }
