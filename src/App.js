334b04041ef0e2e457b7535e9f6f1fec53521a59
Arslan <ali.arslan319@gmail.com>
2022-09-24 22:23:48 -0400

    create Sidebar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import Header from './Header';
+import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
 
 const App = () => {
   return (
     <div className="app">
         <h1>Skylinked</h1>
         <Header />
+
+        <div className='app_body'>
+          <Sidebar />
+        </div>
     </div>
   );
 }
