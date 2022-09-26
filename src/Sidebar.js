bef3c46b3f27adc1f65ada25cf5720256f0f0650
Arslan <ali.arslan319@gmail.com>
2022-09-25 20:08:07 -0400

    stylize Sidebar.js with Sidebar.css

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,9 +6,26 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className="sidebar_top">
         <img src='' alt='' />
-        <Avatar />
+        <Avatar className='sidebar_avatar'/>
         <h2>Arslan Ali</h2>
         <h4>Arslan@gmail.com</h4>
+      </div>
+      <div className="sidebar_stats">
+
+        <div className="sidebar_stat">
+        <p>Who Viewed You</p>
+        <p className='sidebar_statNumber'>999</p>
+        </div>
+
+        <div className="sidebar_stat">
+          <p>Views On Post</p>
+          <p className='sidebar_statNumber'>666</p>
+        </div>
+        
+        <div className="sidebar_bottom">
+          <p>Recent</p>
+        </div>
+
       </div>
     </div>
   )
