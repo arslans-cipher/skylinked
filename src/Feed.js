cae552627f208d81858f2975925acb4b93e7669a
Arslan <ali.arslan319@gmail.com>
2022-10-06 23:41:05 -0400

    import Create icon and establish input field with send button

--- src/Feed.js
+++ src/Feed.js
@@ -1,7 +1,16 @@
+import './Feed.css'
+import CreateIcon from '@mui/icons-material/Create'
 
 const Feed = () => {
   return <div className='feed'>
-    
+    <div className='feed_inputContainer'>
+      <CreateIcon />
+      <form>
+        <input className='text' />
+          <button>Send</button>
+      </form>
+      <div className="feed_input"></div>
+    </div>
   </div>
 }
 
