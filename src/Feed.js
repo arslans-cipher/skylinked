936d6ffc0b1fb151c3c548f6e30052b792ad122e
Arslan <ali.arslan319@gmail.com>
2022-10-06 23:59:10 -0400

    further styling of Feed.css

--- src/Feed.js
+++ src/Feed.js
@@ -4,12 +4,13 @@ import CreateIcon from '@mui/icons-material/Create'
 const Feed = () => {
   return <div className='feed'>
     <div className='feed_inputContainer'>
-      <CreateIcon />
-      <form>
-        <input className='text' />
+      <div className="feed_input">
+        <CreateIcon />
+        <form>
+          <input className='text' />
           <button type='submit'>Send</button>
-      </form>
-      <div className="feed_input"></div>
+        </form>
+      </div>
     </div>
   </div>
 }
