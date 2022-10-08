bfe62d8e480eaacc0713dd6b09d75934c64603fd
Arslan <ali.arslan319@gmail.com>
2022-10-07 23:33:58 -0400

    add more icons from mui and pass props

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,12 @@
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create'
+import ImageIcon from '@mui/icons-material/Image'
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
+import EventNoteIcon from '@mui/icons-material/Event'
+import CalenderViewDayIcon from '@mui/icons-material/CalendarViewDay'
+
+
+import InputOption from './InputOption'
 
 const Feed = () => {
   return <div className='feed'>
@@ -12,7 +19,10 @@ const Feed = () => {
         </form>
       </div>
       <div className='feed_inputOptions'>
-        <InputOption />
+        <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9'/>
+        <InputOption Icon={SubscriptionsIcon} title='Photo' color='#E7A33E'/>
+        <InputOption Icon={EventNoteIcon} title='Photo' color='#C0CBCD'/>
+        <InputOption Icon={CalenderViewDayIcon} title='Photo' color='#7FC15E'/>
       </div>
     </div>
   </div>
--- src/InputOption.js
+++ src/InputOption.js
@@ -1,7 +1,10 @@
 import './InputOption.css'
 
 const InputOption = ({ Icon, title, color}) => {
-    return <div className=''></div>
+    return <div className=''>
+      <Icon style={ { color: color} }/>
+      <h4>{title}</h4>
+    </div>
 }
 
 export default InputOption
