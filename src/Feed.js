7ea77810182cbe0640c56d9a62d873b786575b30
Arslan <ali.arslan319@gmail.com>
2022-10-07 23:47:58 -0400

    .

--- src/Feed.js
+++ src/Feed.js
@@ -19,10 +19,10 @@ const Feed = () => {
         </form>
       </div>
       <div className='feed_inputOptions'>
-        <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9'/>
-        <InputOption Icon={SubscriptionsIcon} title='Photo' color='#E7A33E'/>
-        <InputOption Icon={EventNoteIcon} title='Photo' color='#C0CBCD'/>
-        <InputOption Icon={CalenderViewDayIcon} title='Photo' color='#7FC15E'/>
+        <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9' />
+        <InputOption Icon={SubscriptionsIcon} title='Photo' color='#E7A33E' />
+        <InputOption Icon={EventNoteIcon} title='Photo' color='#C0CBCD' />
+        <InputOption Icon={CalenderViewDayIcon} title='Photo' color='#7FC15E' />
       </div>
     </div>
   </div>
