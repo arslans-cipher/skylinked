4f2fd22b6411c9b3b7e26d9f11b1653b202a43ad
Arslan <ali.arslan319@gmail.com>
2022-10-09 17:48:28 -0400

    add firebase to project

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAEoHDzxFdWVzdpYDa6VGlkUZloH8aIVYA",
+  authDomain: "skylinked-7481c.firebaseapp.com",
+  projectId: "skylinked-7481c",
+  storageBucket: "skylinked-7481c.appspot.com",
+  messagingSenderId: "593776624297",
+  appId: "1:593776624297:web:e3d39452e928df1f2a9fb8",
+  measurementId: "G-PC6TG5WF38"
+}; 
