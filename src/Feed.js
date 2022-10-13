f1f925503c3a892a7fc9eb358f4ae894f76b9cff
Arslan <ali.arslan319@gmail.com>
2022-10-12 22:19:15 -0400

    major refactoring of firebase codebase to make it work in 2022

--- src/Feed.js
+++ src/Feed.js
@@ -8,34 +8,53 @@ import CalenderViewDayIcon from '@mui/icons-material/CalendarViewDay'
 import Post from './Post'
 import InputOption from './InputOption'
 import { db } from './firebase'
-import firebase from './firebase'
+import { onSnapshot, collection, serverTimestamp} from "firebase/firestore"
+
 
 const Feed = () => {
   const [input, setInput] = useState('')
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
-      setPosts(
-        snapshot.doc.map((doc) => ({
-          id: doc.id,
-          doc: doc.data()
-        }))
-      )
+    const abc = collection(db, 'posts')
+    onSnapshot(abc, snapshot => {
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id, 
+        data: doc.data()
+      })))
     })
+
+    // db.collection('posts').onSnapshot((snapshot) => 
+    //   setPosts(snapshot.docs.map((doc) => ({
+    //       id: doc.id,
+    //       doc: doc.data()
+    //     }))
+    //   )
+    // )
   }, [])
 
+  // const orderedOrders = query(ref, orderBy('created', 'desc'))
+  // onSnapshot(orderedOrders, snapshot => {
+  //      setOrders(snapshot.docs.map(doc => ({
+  //        id: doc.id,
+  //        data: doc.data()
+  //      })))
+  //   })
+
   const sendPost = event => {
-    event.preventdefault()
+    console.log("EVENT", event.target.value);
+    event.preventDefault()
     db.collection('posts').add({
       name: 'this is my name',
       description: 'this is a test',
       message: input,
       photoURL: '',
-      timestamp: firebase.firestore.FieldValue.serverTimeStamp()
+      timestamp: serverTimestamp()
     })
+    setInput('')
   }
 
+  console.log('posts', posts)
   return <div className='feed'>
     <div className='feed_inputContainer'>
       <div className="feed_input">
@@ -52,6 +71,7 @@ const Feed = () => {
         <InputOption Icon={CalenderViewDayIcon} title='Photo' color='#7FC15E' />
       </div>
     </div>
+
     {posts.map(({ id, data: { name, description, message, photoURL } }) => (
       <Post
         key={id}
@@ -59,7 +79,8 @@ const Feed = () => {
         description={description}
         message={message}
         photoURL={photoURL}
-      />))}
+      />
+    ))}
   </div>
 }
 
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
-import 'firebase/compat/firestore'
+// import firebase from 'firebase/compat/app'
+// import 'firebase/compat/auth'
+// import 'firebase/compat/firestore'
+import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app"
+import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAEoHDzxFdWVzdpYDa6VGlkUZloH8aIVYA",
@@ -13,9 +16,11 @@ const firebaseConfig = {
   measurementId: "G-PC6TG5WF38"
 }; 
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
-const db = firebaseApp.firestore()
-const auth = firebase.auth()
+// const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig)
+// const db = firebaseApp.firestore()
+const db = getFirestore(firebaseApp)
+// const auth = firebase.auth()
+const auth = getAuth(firebaseApp);
 
 export { db, auth }
-export default firebase
