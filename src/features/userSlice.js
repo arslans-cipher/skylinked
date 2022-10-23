dad4882c0a0c47cd9ad05afffecbf52b9e07c32f
Arslan <ali.arslan319@gmail.com>
2022-10-22 22:58:57 -0400

    syntax error

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 export const userSlice = createSlice({
   name: "user",
@@ -7,16 +7,16 @@ export const userSlice = createSlice({
   },
   reducers: {
     login: (state, action) => {
-      state.user = action.payload;
+      state.user = action.payload
     },
     logout: (state) => {
-      state.user = null;
+      state.user = null
     },
   },
-});
+})
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout } = userSlice.actions
 
-export const selectUser = (state) => state.user;
+export const selectUser = state => state.user
 
-export default userSlice.reducer;
+export default userSlice.reducer
