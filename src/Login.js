77fc6aff972563277b41023c501272d802e1640e
Arslan <ali.arslan319@gmail.com>
2022-10-29 16:21:50 -0400

    registerHandler function

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,26 @@
 import { useState } from 'react'
+import { auth } from './firebase'
 import './Login.css'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
+  const [profilePicture, setProfilePicture] = useState('')
 
 
   const registerHandler = () => {
+    if (!name) {
+      return alert('Please enter a full name')
+    }
 
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(userAuth() => {
+      userAuth.user.updateProfile({
+        displayName: name,
+        profileURL: profilePicture
+      }) 
+    })
   }
 
   const loginHandler = event => {
@@ -24,25 +36,28 @@ const Login = () => {
         placeholder="Full name (required for register)"
         type="text"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(event) => setName(event.target.value)}
       />
 
       <input
         placeholder="Profile pic URL (optional)"
         type="text"
+        value={profilePicture}
+        onChange={(event) => setProfilePicture(event.target.value)}
       />
 
       <input
         placeholder="Email"
         type="email"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(event) => setEmail(event.target.value)}
       />
+
       <input
         placeholder="Password"
         type="password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(event) => setPassword(event.target.value)}
       />
 
       <button type="submit" onClick={loginHandler}>
@@ -56,6 +71,7 @@ const Login = () => {
         Register Now
       </span>
     </p>
+
   </div>
 }
 
